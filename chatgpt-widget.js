e4dd59c10482fc98123283bc2ad22fce4693984a
Hisune <hi@hisune.com>
2023-08-16 15:08:08 +0800

    timestamp format

--- chatgpt-widget.js
+++ chatgpt-widget.js
@@ -500,24 +500,36 @@
       // Reply to the user
       this.sendChatCompletion(this);
     },
+    wrapTimeTitle: function(timestamp, text){
+      const date = new Date(timestamp);
+      const year = date.getFullYear();
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const day = String(date.getDate()).padStart(2, '0');
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
+
+      const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+      return `<span title="${formattedDate}">${text}</span>`;
+    },
     formatTimestamp: function(timestamp) {
       if(!timestamp) timestamp = new Date().getTime();
       const seconds = Math.floor((new Date() - timestamp) / 1000);
 
       let interval = Math.floor(seconds / 31536000);
-      if (interval > 1) return interval + ' years ago';
+      if (interval > 1) return this.wrapTimeTitle(timestamp, interval + ' years ago');
 
       interval = Math.floor(seconds / 2592000);
-      if (interval > 1) return interval + ' months ago';
+      if (interval > 1) return this.wrapTimeTitle(timestamp, interval + ' months ago');
 
       interval = Math.floor(seconds / 86400);
-      if (interval > 1) return interval + ' days ago';
+      if (interval > 1) return this.wrapTimeTitle(timestamp, interval + ' days ago');
 
       interval = Math.floor(seconds / 3600);
-      if (interval > 1) return interval + ' hours ago';
+      if (interval > 1) return this.wrapTimeTitle(timestamp, interval + ' hours ago');
 
       const date = new Date(timestamp);
-      return `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`;
+      return this.wrapTimeTitle(timestamp, `${date.getHours()}:${String(date.getMinutes()).padStart(2, '0')}`);
     },
     ask: function(message, timestamp) {
       message = message.replace(/(?:\r\n|\r|\n)/g, '<br>');
